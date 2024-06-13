e158fbc531fa427b45e416ed2e4a2b6becaf5c46
Jeronimo Balestra <jeronimobalestra@gmail.com>
2024-06-13 18:42:41 -0300

    add github button

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -1,20 +1,13 @@
-import {
-  Book,
-  Bot,
-  Code2,
-  LifeBuoy,
-  Settings2,
-  SquareTerminal,
-  SquareUser,
-  Triangle,
-} from "lucide-react";
+import { Github, SquareTerminal, Triangle } from "lucide-react";
 
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export default function SideBar() {
   return (
@@ -104,20 +97,22 @@ export default function SideBar() {
       <nav className="mt-auto grid gap-1 p-2">
         <Tooltip>
           <TooltipTrigger asChild>
-            <Button
-              variant="ghost"
-              size="icon"
-              className="mt-auto rounded-lg"
+            <Link
+              href={"https://github.com/jero237/chat-playground"}
+              className={cn(
+                "mt-auto rounded-lg",
+                buttonVariants({ variant: "ghost", size: "icon" })
+              )}
               aria-label="Help"
             >
-              <LifeBuoy className="size-5" />
-            </Button>
+              <Github className="size-5" />
+            </Link>
           </TooltipTrigger>
           <TooltipContent side="right" sideOffset={5}>
             Help
           </TooltipContent>
         </Tooltip>
-        <Tooltip>
+        {/* <Tooltip>
           <TooltipTrigger asChild>
             <Button
               variant="ghost"
@@ -131,7 +126,7 @@ export default function SideBar() {
           <TooltipContent side="right" sideOffset={5}>
             Account
           </TooltipContent>
-        </Tooltip>
+        </Tooltip> */}
       </nav>
     </aside>
   );
