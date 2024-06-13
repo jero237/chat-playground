0ec1d1a0e55d7bf4031542bbd46856d6341d804f
Jeronimo Balestra <jeronimobalestra@gmail.com>
2024-06-13 18:48:37 -0300

    refactor: update styling for chat component

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -8,7 +8,7 @@ export default function Chat() {
   const { messages } = useContext(ChatContext);
   return (
     <main className="flex-1 w-full max-w-7xl overflow-auto sm:p-4 mx-auto">
-      <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
+      <div className="relative flex h-full min-h-[50vh] flex-col sm:rounded-xl bg-muted/50 p-4 lg:col-span-2">
         <div className="flex-1 overflow-y-auto pb-2">
           <div className="flex flex-col gap-4">
             {messages.map((message, index) => (
